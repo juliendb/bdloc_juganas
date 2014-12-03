6914993db95b31b82386a444084abecb7976d626
gaele <gaele.lagacherie@gmail.com>
2014-12-03 09:50:07 +0100

    mise à jour user

--- src/Bdloc/AppBundle/Controller/UserController.php
+++ src/Bdloc/AppBundle/Controller/UserController.php
@@ -13,10 +13,17 @@ use Symfony\Component\Security\Core\SecurityContext;
 /**
 	*@Route("/connexion")
 	*/
-	public function loginAction()
-    {/*
+	public function loginAction(Resquest $request)
+    {
+
+    	$params = array();
+
+        $registerForm = $this->createForm(new RegisterType(), $user);
+
+        //gère la soumission du form
         $request = $this->getRequest();
         $session = $request->getSession();
+
         // get the login error if there is one
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
             $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
@@ -28,6 +35,28 @@ use Symfony\Component\Security\Core\SecurityContext;
             // last username entered by the user
             'last_username' => $session->get(SecurityContext::LAST_USERNAME),
             'error'         => $error,
-        ));*/
+        ));
+
+
+        //sauvegarde en bdd avec l'entity manager
+        $em = $this->getDoctrine()->getManager();
+        //elle la sauvegarde en bdd en persistant
+        $em->persist($user);
+        //on excute toutes nos données
+        $em->flush();
+
+		
+        //CONNEXION AUTOMATIQUE : src : http://stackoverflow.com/questions/9550079/how-to-programmatically-login-authenticate-a-user
+        //secured_area est le nom du firewall défini dans security.yml
+        $token = new UsernamePasswordToken($user, $user->getPassword(), "secured_area", $user->getRoles());
+        $this->get("security.context")->setToken($token);
+
+        //déclenche l'evenement de login
+       // $event = new InteractiveLoginEvent($request, $token);
+      //  $this->get("event_dispatcher")->dispatch("security.interactive_login",$event);
+
+        //redirige vers l'accueil
+        return $this->redirect( $this->generateUrl("bdloc_app_subscribe_deliverystep2"));
+
     }
 }
