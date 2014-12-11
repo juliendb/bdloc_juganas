3b1cdfc3fb80b08d6e4b29ba99a9f1d468b5f9de
Deyris <ludovic.deyris@hotmail.fr>
2014-12-11 14:41:49 +0100

    validate edit form profil

--- web/js/address.js
+++ web/js/address.js
@@ -50,6 +50,7 @@ app = {
 			var lon = place.geometry.location.k;
 			var lat = place.geometry.location.D;
 
+			console.log(lon);
 			$("#bdloc_appbundle_user_longitude").val(lon)
 			$("#bdloc_appbundle_user_latitude").val(lat)
 	
